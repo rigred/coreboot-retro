e496b387c418599397f346cb0ce949a366b56afb
rigred <rigo.reddig@gmail.com>
2023-02-15 09:14:55 +0200

    Introduce W83977EF as seperate device to work around issues without touching W83977TF tree
    Signed-off-by: Rigo Reddig <rigo.reddig@gmail.com>

---
+++ src/superio/winbond/w83977ef/w83977ef.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#ifndef SUPERIO_WINBOND_W83977EF_W83977EF_H
+#define SUPERIO_WINBOND_W83977EF_W83977EF_H
+
+#define W83977EF_FDC              0   /* Floppy */
+#define W83977EF_PP               1   /* Parallel port */
+#define W83977EF_SP1              2   /* Com1 */
+#define W83977EF_SP2              3   /* Com2 */
+#define W83977EF_KBC              5   /* PS/2 keyboard & mouse */
+#define W83977EF_CIR              6
+#define W83977EF_GAME_MIDI_GPIO1  7
+#define W83977EF_GPIO2            8
+#define W83977EF_ACPI            10
+
+#endif
