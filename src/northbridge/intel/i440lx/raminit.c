51249ac116d01e285e66f6ef1fcfd32ce781bfb8
rigred <rigo.reddig@gmail.com>
2023-12-07 14:05:02 +0200

    440LX: fix end ofline close

--- src/northbridge/intel/i440lx/raminit.c
+++ src/northbridge/intel/i440lx/raminit.c
@@ -345,7 +345,7 @@ static void set_chipset_regs(void) {
 	/* TODO? */
 	pci_write_config8(NB, PCI_LATENCY_TIMER, 0x20);
 	pci_write_config8(NB, MTT, 0x20);
-	pci_write_config8(NB, AMTT, 0x20)
+	pci_write_config8(NB, AMTT, 0x20);
 }
 
 /*-----------------------------------------------------------------------------
