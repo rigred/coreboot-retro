ea3a28811a73ec8fa7f9438b264e16baff39ac29
rigred <rigo.reddig@gmail.com>
2023-12-10 23:56:52 +0200

    Winbond SIO remove set clock func

--- src/mainboard/msi/ms6117/bootblock.c
+++ src/mainboard/msi/ms6117/bootblock.c
@@ -8,8 +8,6 @@
 
 void bootblock_mainboard_early_init(void)
 {
-	winbond_set_clksel_48(SERIAL_DEV);
-	winbond_read_pnp_reg(SERIAL_DEV);
 	winbond_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
 	winbond_read_pnp_reg(SERIAL_DEV);
 }
