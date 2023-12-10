61099518dd630a9a8bd97ec0e8b0667c3342a7d2
rigred <rigo.reddig@gmail.com>
2023-12-10 23:00:26 +0200

    MS6117: set 48Mhz clock

--- src/mainboard/msi/ms6117/bootblock.c
+++ src/mainboard/msi/ms6117/bootblock.c
@@ -4,9 +4,10 @@
 #include <superio/winbond/common/winbond.h>
 #include <superio/winbond/w83977tf/w83977tf.h>
 
-#define SERIAL_DEV PNP_DEV(0x3f0, W83977TF_SP1)
+#define SERIAL_DEV PNP_DEV(0x3f0, W83977TF_SP2)
 
 void bootblock_mainboard_early_init(void)
 {
 	winbond_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
+	winbond_set_clksel_48(SERIAL_DEV);
 }
